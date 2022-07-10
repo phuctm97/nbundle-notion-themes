dae1472f7c2b3550e33fed0ea7eaf73cd3add9ef
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2022-07-10 16:43:37 +0800

    fix: use latest API

--- app/index.jsx
+++ app/index.jsx
@@ -1,18 +1,80 @@
-import { useCallback } from "react";
 import { TopbarMenuItem } from "@nbundle/react";
-import { AiOutlineFormatPainter } from "react-icons/ai";
+import { AiOutlineFontSize, AiOutlineFormatPainter } from "react-icons/ai";
 
 export default function App() {
-  const setCustomTheme = useCallback(() => {
-    console.log("Select custom theme");
-  }, []);
   return (
     <>
       <TopbarMenuItem
-        type="button"
+        type="select"
         text="Set custom theme"
         icon={AiOutlineFormatPainter}
-        onClick={setCustomTheme}
+        value="Notion Default"
+        optionsTitle="Set theme"
+        options={[
+          "Notion Default",
+          "Laser Wave",
+          "Material Ocean",
+          "Dracula",
+          "Deep Dark",
+          "Github Light",
+          "Github Dark",
+          "Kawaii",
+          "Yotsuba",
+          "Base 16 Eighties",
+          "Library",
+        ]}
+      />
+      <TopbarMenuItem
+        type="select"
+        text="Set custom font"
+        icon={AiOutlineFontSize}
+        value="Notion Default"
+        optionsTitle="Set font"
+        options={[
+          "Notion Default",
+          "Arimo",
+          "Barlow",
+          "Dosis",
+          "Fira Sans",
+          "Heebo",
+          "IBM Plex Sans",
+          "Inter",
+          "Josefin Sans",
+          "Karla",
+          "Libre Franklin",
+          "Lora",
+          "Merriweather",
+          "Montserrat",
+          "Mukta",
+          "Mulish",
+          "Nanum Gothic",
+          "Noto Sans",
+          "Noto Serif",
+          "Nunito",
+          "Nunito Sans",
+          "Open Sans",
+          "Oswald",
+          "Oxygen",
+          "Poppins",
+          "PT Sans",
+          "Playfair Display",
+          "Raleway",
+          "PT Serif",
+          "Roboto",
+          "Roboto Condensed",
+          "Roboto Mono",
+          "Roboto Slab",
+          "Rubik",
+          "Source Code Pro",
+          "Source Sans Pro",
+          "Titillium Web",
+          "Ubuntu",
+          "Work Sans",
+          "Yanone Kaffeesatz",
+          "Lato",
+          "Abel",
+          "Exo 2",
+        ]}
       />
     </>
   );
